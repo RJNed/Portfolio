ba81ccacca4cb5448a3bcaea61dc7a22f483277f
Ryan <rjnedbalek@gmail.com>
2025-04-13 11:53:34 -0500

    Please build

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar">
       <!-- Logo or Brand -->
       <div class="brand">
-        <a href="/ "><img src="../src/Logos/portfolio-icon.png"></img></a>
+        <a href="/ "><img src="/imagesportfolio-icon.png"></img></a>
       </div>
       
       <!-- Navigation Links -->
